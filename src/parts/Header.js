6e3295026a17f52d54b5288ea8a3734b1a838214
racmathafidz <rac.hafidz@gmail.com>
2021-04-07 18:25:57 +0700

    add header

---
+++ src/parts/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import BrandIcon from 'parts/BrandIcon'
+
+export default function Header(props) {
+    return (
+        <div>
+            <BrandIcon/>
+        </div>
+    )
+}
