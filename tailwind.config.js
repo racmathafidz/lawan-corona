2f3963f930fc914e7ee594f66312065c227beb5b
racmathafidz <rac.hafidz@gmail.com>
2021-04-07 18:23:49 +0700

    customizing font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   variants: {
