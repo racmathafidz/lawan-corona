1b3592e6e6a481dddaa73ec94fa16a424eb3c580
racmathafidz <rac.hafidz@gmail.com>
2021-04-07 18:25:47 +0700

    add brand icon

---
+++ src/parts/BrandIcon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function BrandIcon() {
+    return (
+        <Button type="link" href="/">
+            <h1 className="text-gray-700 text-4xl font-semibold tracking-tight leading-none">Lawan</h1>
+            <h1 className="text-red-500 text-4xl font-semibold tracking-tight leading-none">Corona</h1>
+        </Button>
+    )
+}
