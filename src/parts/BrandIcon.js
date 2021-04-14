bb0d3da04565462731f16ad26c68d8c4cee5f905
racmathafidz <rac.hafidz@gmail.com>
2021-04-14 19:48:52 +0700

    add Logo

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -5,8 +5,8 @@ import Button from 'elements/Button'
 export default function BrandIcon() {
     return (
         <Button type="link" href="/">
-            <h1 className="text-gray-700 text-4xl font-semibold tracking-tight leading-none">Lawan</h1>
-            <h1 className="text-red-500 text-4xl font-semibold tracking-tight leading-none">Corona</h1>
+            <h1 className="text-gray-800 text-3xl font-semibold tracking-tight leading-none">Lawan</h1>
+            <h1 className="text-red-600 text-3xl font-semibold tracking-tight leading-none">Corona</h1>
         </Button>
     )
 }
