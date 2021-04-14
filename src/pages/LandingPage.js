235116895d7ea2af261267ee5b36a300e112abfe
racmathafidz <rac.hafidz@gmail.com>
2021-04-14 19:48:39 +0700

    add fetch API and Redux store

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Header from 'parts/Header'
+import Hero from 'parts/Hero'
+import CountryStatistic from 'parts/CountryStatistic'
 
-export default class LandingPage extends Component {
+import { fetchCountry } from 'store/actions/country'
+import { fetchDate } from 'store/actions/date'
+
+class LandingPage extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount(){
+        window.scrollTo(0, 0)
+
+        if (this.props.country === null && this.props.date === null) {
+            this.props.fetchCountry()
+            this.props.fetchDate()
+        }
+    }
+
     render() {
+
+        if (this.props.country === null && this.props.date === null) {
+            return (
+                <div>
+                    <Header {...this.props}/>
+                    <Hero {...this.props}/>
+                </div>
+            )
+        }
+
         return (
             <div>
                 <Header {...this.props}/>
+                <Hero {...this.props}/>
+                <CountryStatistic data={this.props.country[0]} date={this.props.date} withButton/>
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => ({
+    country: state.country,
+    date: state.date
+})
+
+export default connect( mapStateToProps, {fetchCountry, fetchDate})(LandingPage)
