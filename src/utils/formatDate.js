504d26bd6a2ab634dce5f03903f7ec600647fc32
racmathafidz <rac.hafidz@gmail.com>
2021-04-14 19:47:41 +0700

    add formating date & number function

---
+++ src/utils/formatDate.js
@@ -0,0 +1,15 @@
+export default date => {
+    const d = new Date(date)
+    const dtf = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit",
+        hour: 'numeric', 
+        minute: 'numeric',
+        hour12: false,
+    })
+    const [{value: mo}, , {value: da}, , {value: ye}, , {value: ho}, , {value: mi}] = dtf.formatToParts(d)
+
+    return `tanggal ${da} ${mo} ${ye} jam ${ho}:${mi}`
+    //return dtf
+}
