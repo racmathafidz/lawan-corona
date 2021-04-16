1bc86c3db5837287044b971ec0bea7b651d2d2ea
racmathafidz <rac.hafidz@gmail.com>
2021-04-16 22:26:24 +0700

    add 404 not found page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import LandingPage from 'pages/LandingPage'
+import StatistikPage from 'pages/StatistikPage'
+import FaqPage from 'pages/FaqPage'
+import NotFoundPage from 'pages/NotFoundPage'
 
 import 'assets/css/styles.css';
 
@@ -8,6 +11,9 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage}/>
+      <Route exact path="/statistik" component={StatistikPage}/>
+      <Route exact path="/faq" component={FaqPage}/>
+      <Route path="" component={NotFoundPage}></Route>
     </Switch>
   );
 }
